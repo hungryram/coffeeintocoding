c12f0c9f98a4f5d2c48e3abb10307f531c91ae43
Ram Dettmer <ramdettmer751@gmail.com>
2020-06-11 01:34:41 -0700

    updating home blog

--- content/contact/_index.md
+++
@@ -1,8 +0,0 @@
-+++
-contact_content = "We look forward to connecting with you."
-title = "Contact Me"
-[menu.main]
-name = "Contact"
-weight = 4
-
-+++
--- static/js/main.js
+++ static/js/main.js
@@ -23,85 +23,11 @@ $(document).ready(function () {
       });
 });
 
-// TESTIMONIAL SLIDER
-$('.responsive-testimonial-slide').slick({
-  dots: true,
-  infinite: true,
-  speed: 800,
-  slidesToShow: 1,
-  autoplay: true,
-  pauseOnHover: true,
-});
-
-// SINGLE LISTING SLIDER
-$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  autoplay: true,
-  adaptiveHeight: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 6,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: false,
-  centerMode: true,
-  focusOnSelect: true
-});
-
-// LIGHTBOX
-$('.slider-for').slickLightbox();
-
-// PROJECTS
-$('.projectmain-slide').slick({
-  dots: true,
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 1,
-  autoplay: true,
-  slidesToScroll: 3,
-  centerMode: true,
-  variableWidth: true,
-  pauseOnHover: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
-
 $('.responsive-slide').slick({
   dots: true,
   infinite: true,
   speed: 800,
-  slidesToShow: 3,
+  slidesToShow: 4,
   autoplay: true,
   slidesToScroll: 3,
   pauseOnHover: true,
